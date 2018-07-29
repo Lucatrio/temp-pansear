0da706fcd17e8d0dbe22a7f4554d36ade55d8e5c
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-29 16:33:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", () => {
+  console.log('yeet yeet potato skeet');
   client.user.setActivity('lucatrio update me', { type: 'WATCHING' })
 });
 client.login(process.env.TOKEN)
